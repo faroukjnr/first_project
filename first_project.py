12eb7ccf117a4867950c803e877a65e47f91660a
faroukjnr <farouk60@gmail.com>
2022-04-11 16:36:46 +0000

    first project

---
+++ first_project.py
@@ -0,0 +1,9 @@
+print (".....WELCOME FAITHFUL USER....")
+print (" kindly answer the question below before proceeding to the site. Thank you")
+first_name = input ("What is your first name?........")
+last_name = input("What is your last name?...........")
+age = int  (input("What is your age?...."))
+if  age >=  18 :
+     print("Heya, you are eligible to use this site")
+else :
+ 	print ("Sorry, you are not eligible to use this site")
